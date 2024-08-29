52ce819bfd35b5b8a436288e40ffd06b6abc9edc
Mohamed Khaled <109092116+mohamedkbx@users.noreply.github.com>
2024-08-29 09:31:44 +0300

    [2629] [Time Beats: 76.24%] [Memory Beats: 72.12%] - LeetPush

---
+++ Day-8/2629-Function-Composition.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+
+ var compose = function(functions) {
+    if (functions.length === 0) {
+     return function(x) { return x; };
+
+    } else {
+        
+    return function(x) {
+    //   var result = functions[functions.length - 1](x);
+    //   for (var i = functions.length - 2; i >= 0; i--) {
+    //     result = functions[i](result);
+    return functions.reduceRight((result, fn) => fn(result), x);
+      
+    //   return result;
+    };
+  }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
